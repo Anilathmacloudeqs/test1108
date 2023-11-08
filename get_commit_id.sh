4a9edfd6c5b76d1471e3f5d76b4e9091d942897b
Anilathmacloudeqs <129284639+Anilathmacloudeqs@users.noreply.github.com>
2023-11-08 12:23:22 +0530

    Create shell script get_commit_id.sh

---
+++ get_commit_id.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+TAG="p1.0"
+FILE="main.py"
+
+# Get the commit ID of the latest commit that modified main.py and has the specified tag
+COMMIT_ID=$(git log -n 1 --pretty=format:"%H" --grep="$TAG" -- $FILE)
+
+echo "Commit ID for $FILE with tag $TAG: $COMMIT_ID"
