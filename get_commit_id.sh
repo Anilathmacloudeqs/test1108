0a177707e61b0c644428c382fcf94d9985f6c76f
Anilathmacloudeqs <129284639+Anilathmacloudeqs@users.noreply.github.com>
2023-11-08 12:36:09 +0530

    Update get_commit_id.sh

--- get_commit_id.sh
+++ get_commit_id.sh
@@ -4,6 +4,10 @@ chmod +x get_commit_id.sh
 TAG="p1.0"
 FILE="main.py"
 
+# Debug information
+echo "Current directory: $(pwd)"
+echo "List files in the current directory: $(ls)"
+
 # Get the commit ID of the latest commit that modified main.py and has the specified tag
 COMMIT_ID=$(git log -n 1 --pretty=format:"%H" --grep="$TAG" -- $FILE)
 
