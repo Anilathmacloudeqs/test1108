5345aad345f29085f7deb964b7d7ba1bfbd5d8b6
Anilathmacloudeqs <129284639+Anilathmacloudeqs@users.noreply.github.com>
2023-11-08 13:37:24 +0530

    Update newshell.ps1

--- newshell.ps1
+++ newshell.ps1
@@ -1,40 +1,18 @@
-<#
- Description: PowerShell Script to get commit id for a specific tag and set it to a pipeline variable
- Author: tres
-#>
-
-param (
-    # Name of the Source Tag
-    $SourceTag
-)
-
-# Set Git global configurations
-git config --global user.name 'tres'
-git config --global user.email 'tres@gmail.com'
-git config --global pull.rebase false
-
-# List current Git configurations and branches
+git config --global user.name 'Anilathmacloudeqs' 
+git config --global user.email 'anilathma@cloudeqs.com'
+git config pull.rebase false 
 git config --list
 git branch
+Write-Host "testing the git tag list"
+git tag
+Write-Host "testing after git tag"
+Write-Host "##change Executing git pull on $($env:SourceTag)"
+git pull origin $env:SourceTag --allow-unrelated-histories
 
-# Display a message
-Write-Host "Testing the Git tag list"
+Write-Host "getting commit id for $($env:SourceTag)"
+$commitid = (git log -n 1 --pretty=format:"%H")
 
-# List Git tags
-$tags = git tag
-Write-Host "Git tags: $($tags -join ', ')"
+Write-Host "Commit ID: $commitid"
 
-# Display a message
-Write-Host "Executing git pull on $SourceTag"
-
-# Perform a Git pull operation on the specified source tag
-git pull origin $SourceTag --allow-unrelated-histories
-
-# Get the commit ID for the specified source tag
-$commitid = (git log -n 1 --pretty=format:%H) $SourceTag
-
-# Display the obtained commit ID
-Write-Host "Commit ID for $SourceTag: $commitid"
-
-# Set the commit ID as a pipeline variable
-echo "COMMIT_ID=$commitid" >> $env:GITHUB_ENV
+# Set the commit id as an environment variable
+$env:COMMIT_ID = $commitid
